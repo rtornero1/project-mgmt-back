d1e446f9a6999bb8263e0db3e0ef31525421e400
rtornero1 <rtornero@chicagobooth.edu>
2023-03-08 16:08:54 +0000

    migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_03_08_160559) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "task_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "caption", null: false
+    t.integer "comments_count", default: 0
+    t.bigint "owner_id", null: false
+    t.string "status", default: "backlog"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["owner_id"], name: "index_tasks_on_owner_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.citext "username"
+    t.integer "comments_count", default: 0
+    t.integer "tasks_count", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "tasks", "users", column: "owner_id"
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :bigint           not null
+#  task_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_author_id  (author_id)
+#  index_comments_on_task_id    (task_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (author_id => users.id)
+#  fk_rails_...  (task_id => tasks.id)
+#
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
